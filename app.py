e6b3241f39316f7e497a3a6693c0d1f45e84c990
Zerkley <miguelmr94@gmail.com>
2023-06-08 23:23:13 +0100

    done

---
+++ app.py
@@ -0,0 +1,10 @@
+#who, action, what, where
+import random
+who = ["My grandma","my dog","my mum","my pet lizard"]
+action = ["ate","shit on","murdered","doinked"]
+what = ["my homework","my keys","my marriage","my grandpa","my will to live"]
+where = ["last night","yesterday","while I was in church","while I was peeing out the window"]
+
+
+
+print(random.choice(who), random.choice(action), random.choice(what), random.choice(where))
